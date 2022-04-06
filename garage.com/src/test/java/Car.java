21e5258355f36d3d917fd2a126a0f800527c058b
lioneldesroses <lioneldesroses@localhost>
2022-04-06 17:27:28 -0400

    clean up the code make it look neat

--- garage.com/src/test/java/Car.java
+++ garage.com/src/test/java/Car.java
@@ -1,53 +1,38 @@
 
- 
+
 public class Car {
 	public Car() {
 		this.model = "Corvette";
-	
 		
-		
-		
-	}
+		}
 	
 	
 	private int odometer;
-	
 	public String model;
-	
 	public String engine;
-	
-	
+
+
 	public void StartEngine() {
-		
 		}
 	public void StopEngine() {
-		
 		}
-	
-
 
 	public int getOdometer() {
 		return odometer;
 		}
-	
-}
 
+}
 
 //interface
 interface IEngine {
-    
-    
-    final int CylinderCount =0;
-  
-    
-    void Start ();
+
+	final int CylinderCount =0;
+	void Start ();
 	}
-  
-abstract 
-class LargeEngine implements IEngine {
-    
-    
-    public void Start(){ 
-      System.out.println("Engine Started"); 
-    }
+
+abstract class LargeEngine implements IEngine {
+
+	public void Start(){ 
+		System.out.println("Engine Started"); 
+	}
 }
