af8977ad6303843ff130c881ac91b7229a08bd28
lioneldesroses <lioneldesroses@localhost>
2022-04-13 17:49:19 -0400

    update hashMap file. should run now

---
+++ garage.com/src/test/java/CSVTest.java
@@ -0,0 +1,35 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class CSVTest {
+  @Test
+  public void f() {
+	  String path ="//Users//lioneldesroses//git/QuintrixTraining/garage.com//src//test//resources//basses.csv";
+	  String C =null;
+	  BufferedReader Base = null;
+	  String Make[] = null;
+	  String Model[] = null;
+	  try {
+		Base  = new BufferedReader (new FileReader(path));
+		while((C=Base.readLine())!= null) {
+			Make = 	C.split(",");
+			Model = C.split(",");
+			//System.out.printf(Make[0],Model[1]);
+			HashMap<String,String> newtest = new HashMap<String,String> ();
+			newtest.put(Make[0], Model[1]);
+			System.out.println(newtest);
+		}	
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} 
+	  assertEquals(C, C,"THE FILE WAS INCORECT");  
+  }
+}
