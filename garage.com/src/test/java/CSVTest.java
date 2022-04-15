6e0687df83d070308071e229dd04f9ed86562462
lioneldesroses <lioneldesroses@localhost>
2022-04-15 14:49:59 -0400

    UPdate on the assertion

--- garage.com/src/test/java/CSVTest.java
+++ garage.com/src/test/java/CSVTest.java
@@ -33,8 +33,8 @@ public class CSVTest {
 		}	
 	} catch (FileNotFoundException e) {
 		e.printStackTrace();	
-	} catch (IOException e) {
-		e.printStackTrace();
+	} catch (IOException I) {
+		I.printStackTrace();
 	} 
 	  
 	System.out.println(ExpectedMap);
