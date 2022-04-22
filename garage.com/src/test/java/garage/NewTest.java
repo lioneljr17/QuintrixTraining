1079dc0ff2ef3346502e22a0fbff3840c8893afa
lioneldesroses <lioneldesroses@192.168.1.251>
2022-04-22 14:03:03 -0400

    change was made according to the comment

--- garage.com/src/test/java/garage/NewTest.java
+++ garage.com/src/test/java/garage/NewTest.java
@@ -1,5 +1,7 @@
 package garage;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
@@ -9,7 +11,7 @@ import org.testng.annotations.Test;
 public class NewTest {
 	
 	@Test
-	public void f1() {
+	public void comparesValueTypesUsingIsEqualToOperator() {
 		  String Data1 = "test";
 		  String Data2 = "test";
 		  
@@ -19,19 +21,19 @@ public class NewTest {
 	  }
 	
   @Test
-  public void f2() {
+  public void comparesReferenceTypesUsingIsEqualToOperator() {
 	  String Data1 = new String("this is 1");
-	  String Data2 = new String  ("this is not 1");
+	  String Data2 = new String("this is not 1");
 	 
 	  boolean Should = Data1 ==Data2;
 
-	  assertEquals( false, Should,"the boolean was true");  
+	  assertEquals(false, Should,"the boolean was true");  
   	   }
   
   @Test
-  public void f3() {
+  public void comparesReferenceTypesUsingEqualsMethod() {
 	  String Data1 = new String("this is 1");
-	  String Data2 = new String  ("this is 1");
+	  String Data2 = new String("this is 1");
 	  
 	  boolean result = Data1.equals(Data2);
 	  
@@ -39,7 +41,7 @@ public class NewTest {
   	   }
   
   @Test
-  public void f4() {
+  public void compareUsinglogicoperation() {
 	 String fun ="hi";
 	 String expected = "having fun";
 	 int x = 1;
@@ -54,7 +56,7 @@ public class NewTest {
   	 }
   
   @Test
-  public void f5() {
+  public void compareusinglogicoperation() {
 	  String fun ="hi";
 		 String expected = "having fun";
 		 int x = 1;
@@ -67,7 +69,7 @@ public class NewTest {
 		 assertEquals(fun, expected,"the string 'fun' did not get replace");
   	   }
   @Test
-  public void f6() {
+  public void numbersArePostIncrementedanddecremented() {
 	  int ManyStudents= 5;
 	  int Manyteacher =5 ;
 	  
@@ -79,7 +81,7 @@ public class NewTest {
 	  
   	  }
   @Test
-  public void f7() {
+  public void creatingabooleantobefalse() {
 	 String MyName= "lionel";
 	 String Name ="";
 	 
@@ -93,7 +95,7 @@ public class NewTest {
   	 }
   
   @Test
-  public void f8() {
+  public void compareusingifoperator() {
 	  int AmountOfWeek =6;
 	  int Day = 0;
 	  
@@ -105,7 +107,7 @@ public class NewTest {
     }
   
   @Test
-  public void f9() {
+  public void compareusingifelseoperator() {
 	  int AmountOfWeek =6;
 	  int Day = 0;
 	  
@@ -120,7 +122,7 @@ public class NewTest {
       }
   
   @Test
-  public void f10() {
+  public void compareusingloop() {
 	  int x =5;
 	  int y =0;
 	  
@@ -131,22 +133,24 @@ public class NewTest {
     }
   
   @Test
-  public void f11() {
+  public void comparewithexeption() {
 	  int FirstNum= 50;
 	  int secondNum =0;
-	  int result = 0;
+	  boolean doestthrowexception = false;
+	  
 	  
 		  try {
-			  result = FirstNum/secondNum;
+			  int result = FirstNum/secondNum;
 		  }
 		  catch(ArithmeticException NOT) {
 			  System.out.println("you cannot divid");
+			  doestthrowexception = true;
 		  } 
-	  assertEquals(secondNum, result, "numbers could not be divided.");  		
+	  assertTrue(doestthrowexception,"exection was not throw");  		
   	 }  
   
   @Test
-  public void f12() {
+  public void compareusingarraylistloop() {
 	  List<String> weeks = Arrays.asList("week1","week2","week3");
 	  int weeksnumber =0;
 	  for (int i= 0;i< weeks.size();i++) {
